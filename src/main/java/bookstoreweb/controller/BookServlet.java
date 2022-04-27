80452190a4190c4ebcbb8823f510d4021af7dde5
Ren <101141319+RenanSM@users.noreply.github.com>
2022-04-27 08:09:52 -0300

    Update BookServlet.java

--- src/main/java/bookstoreweb/controller/BookServlet.java
+++ src/main/java/bookstoreweb/controller/BookServlet.java
@@ -51,6 +51,18 @@ public class BookServlet extends HttpServlet {
                     insertBookAction(request, response);
                     break;
                 
+                case "edit":
+                    showEditBookForm(request, response);
+                    break;
+                
+                case "delete":
+                    deleteBookAction(request, response);
+                    break;
+                    
+                case "update":
+                    updateBookAction(request, response);
+                    break;
+                
                 case "/list":    
                 default:
                     listBook(request, response);
